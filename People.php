f6b9ae750425ab64e0e1a56cd6b7c90521ac7577
cecep29 <cecepefendi.c2p@gmail.com>
2020-05-28 06:01:04 +0700

    initial commit

---
+++ People.php
@@ -0,0 +1,107 @@
+<?php 
+
+interface People 
+{
+    public function doSomething();
+}
+
+interface Worker extends People 
+{
+    public function doJob();
+}
+
+interface Student extends Worker 
+{
+    public function doExtraSomething();
+}
+
+class RemoteWorker implements Worker 
+{
+    public function doSomething()
+    {
+        return 'do something from '.__CLASS__;
+    }
+
+    public function doJob()
+    {
+        return 'do job from '.__CLASS__;
+    }
+}
+
+class FactoryWorker implements Worker 
+{
+    public function doSomething()
+    {
+        return 'do something from '.__CLASS__;;
+    }
+
+    public function doJob()
+    {
+        return 'do job from '.__CLASS__;;
+    }
+}
+
+class CollegeStudent 
+{
+    public function doSomething()
+    {
+        return 'do something from '.__CLASS__;;
+    }
+
+    public function doExtraSomething()
+    {
+        return 'do extra something from '.__CLASS__;;
+    }
+
+    public function doJob()
+    {
+        return 'do job from '.__CLASS__;;
+    }
+}
+
+class HighSchoolStudent 
+{
+    public function doSomething()
+    {
+        return 'do something from '.__CLASS__;;
+    }
+
+    public function doExtraSomething()
+    {
+        return 'do extra something from '.__CLASS__;;
+    }
+
+    public function doJob()
+    {
+        return 'do job from '.__CLASS__;;
+    }
+}
+
+
+$remote = new RemoteWorker();
+echo $remote->doSomething();
+echo PHP_EOL;
+echo $remote->doJob();
+echo PHP_EOL;
+echo PHP_EOL;
+
+$factory = new FactoryWorker();
+echo $factory->doSomething();
+echo PHP_EOL;
+echo $factory->doJob();
+echo PHP_EOL;
+echo PHP_EOL;
+
+$college = new CollegeStudent();
+echo $college->doSomething();
+echo PHP_EOL;
+echo $college->doJob();
+echo PHP_EOL;
+echo PHP_EOL;
+
+$highschool = new CollegeStudent();
+echo $highschool->doSomething();
+echo PHP_EOL;
+echo $highschool->doJob();
+echo PHP_EOL;
+echo PHP_EOL;
